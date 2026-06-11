07f4abebeedca73bfa064768c0f6e28be68e60b6
Ishrat0628 <mdrazamakandar1@gmail.com>
2026-06-11 18:05:35 +0530

    new

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello Joel sir")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hi from docker"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
